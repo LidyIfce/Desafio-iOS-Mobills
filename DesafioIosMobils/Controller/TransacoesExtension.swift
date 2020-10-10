eec8e2b863803e1005be8148080315787fb86145
Lidiane <lidianegomesbarbosa@gmail.com>
2020-10-10 08:25:08 -0300

    reorgazinação de código

--- DesafioIosMobils/Controller/TransacoesExtension.swift
+++ DesafioIosMobils/Controller/TransacoesExtension.swift
@@ -14,7 +14,7 @@ extension TransacoesViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TransacoesTableViewCell.reuseIdentifier) as? TransacoesTableViewCell else { return UITableViewCell() }
         cell.transacao = transacoes[indexPath.row]
-       
+        
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -22,13 +22,11 @@ extension TransacoesViewController: UITableViewDelegate, UITableViewDataSource {
         guard let viewC = storyboard.instantiateViewController(identifier: "novatransacao") as? NovaTransacaoViewController else { fatalError() }
         viewC.modalPresentationStyle = .fullScreen
         viewC.transacao = transacoes[indexPath.row]
-        print(transacoes[indexPath.row].uid)
         present(viewC, animated: true)
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-
             TransacaoService.shared.removeTransacao(transacao: transacoes[indexPath.row]) { (error, ref) in
                 if let error = error {
                     print(error.localizedDescription)
